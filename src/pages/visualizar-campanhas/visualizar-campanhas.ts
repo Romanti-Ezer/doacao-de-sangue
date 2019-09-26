c499ed3f2b2e444dddd6a720f830ce4f3770fb0e
Guilbert <guilbertoliveira@hotmail.com>
2019-09-26 13:29:37 -0300

    Incluindo o google maps
    Consegui incluir o google maps porém não consigo colocar no ion-card, estarei tentando colocar busca pelo address

--- src/pages/visualizar-campanhas/visualizar-campanhas.ts
+++ src/pages/visualizar-campanhas/visualizar-campanhas.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Observable } from 'rxjs';
 import { FirestoneService } from '../../services/firestone.service';
 import { Campaign } from '../../services/firestone.service';
+declare var google: any;
+
+
+
 
 /**
 * Generated class for the VisualizarCampanhasPage page.
@@ -16,17 +20,14 @@ import { Campaign } from '../../services/firestone.service';
     selector: 'page-visualizar-campanhas',
     templateUrl: 'visualizar-campanhas.html',
 })
+
 export class VisualizarCampanhasPage {
-    
     protected campaigns: Observable<Campaign[]>;
     protected state: string = '';
-    
+    @ViewChild('map') map: ElementRef;
+  
     constructor(public navCtrl: NavController, public navParams: NavParams, public firestone: FirestoneService) {
         this.campaigns = this.firestone.getCampaign();
-    }
-    
-    public ionViewDidLoad() {
-        console.log('ionViewDidLoad VisualizarCampanhasPage');
     }
     
     public viewMore(event) {
@@ -50,4 +51,27 @@ export class VisualizarCampanhasPage {
         }
         return null;
     };
+
+    ionViewDidLoad() {
+      this.showMap();
+    }
+  
+    showMap() {
+      const position = new google.maps.LatLng(-23.5505, -46.6333);
+  
+      const options = {
+        center: position,
+        zoom: 10,
+        mapTypeId: 'hybrid'
+      };
+  
+      const map = new google.maps.Map(this.map.nativeElement, options);
+  
+      var marker = new google.maps.Marker({
+        position: position,
+        map: map
+      });
+    }
+
+
 }
