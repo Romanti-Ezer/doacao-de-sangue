85d8527207a8025dcc4e039f08e02ffa4fd26fa5
Romanti-Ezer Santos <romanti.santos@fatec.sp.gov.br>
2019-10-25 13:13:25 -0300

    Arquivo exemplo com credenciais criado

---
+++ src/app/config.example.ts
@@ -0,0 +1,17 @@
+export const credentials = {
+    firebase: {      
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: ""
+    },
+    dialogflow: {
+        accessToken: ""
+    },
+    googleCloud: {
+        api: ""
+    }
+};
